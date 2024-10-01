46314a20075682a556fe17767778ccc55d4daf9f
kiet-444 <kietcao444@gmail.com>
2024-10-02 00:39:30 +0700

    fix

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(upload.any());
 const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.js"
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
     customCss: '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
-    customJs: CSS_URL
+    customCssUrl: CSS_URL
 }));
 
 
