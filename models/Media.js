a4af92a088eee83f13767c6a911e498f23ee349d
kiet-444 <kietcao444@gmail.com>
2024-10-01 19:28:01 +0700

    a

--- models/Media.js
+++ models/Media.js
@@ -6,7 +6,7 @@ const mediaSchema = new mongoose.Schema({
     url: { type: String, required: true },
     created_at: { type: Date, required: true }
 });
-
+//abc
 const Media = mongoose.model('Media', mediaSchema);
 
 module.exports = Media;
