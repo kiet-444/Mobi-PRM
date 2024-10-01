3d1f6316e98f91308bdd5af9a576b6dc1bb9b591
kiet-444 <kietcao444@gmail.com>
2024-10-01 19:05:14 +0700

    db

--- config/connectDB.js
+++ config/connectDB.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 async function connectDB() {
     try {
         await mongoose.connect(process.env.MONGO_DB, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            
         })
         console.log('Connect successfully')
     } catch (error) {
